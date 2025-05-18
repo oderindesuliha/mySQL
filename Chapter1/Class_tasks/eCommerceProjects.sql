94751e8074334a5c1754644485b26a38cecd40e2
Oderinde Suliha <oderindesuliha@gmail.com>
2025-05-18 17:28:59 +0100

    first commit

---
+++ Chapter1/Class_tasks/eCommerceProjects.sql
@@ -0,0 +1,9 @@
+
+
+
+INSERT INTO orders (user_id, order_date, total_amount, quantity, status, product_id) VALUES
+(1, NOW(), 120.50, 2, 'pending', 3),
+(2, NOW(), 250.00, 5, 'shipped', 1),
+(3, NOW(), 89.99, 1, 'pending', 4),
+(4, NOW(), 300.75, 3, 'delivered', 2),
+(5, NOW(), 150.00, 2, 'canceled', 5);
